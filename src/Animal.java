d04ac801868a55b6c3067086108a4506c3277741
a25elenapg <htmtf111@gmail.com>
2025-12-02 17:38:03 +0100

    Class Animal

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,22 @@
 public abstract class Animal {
+  private String nombre;
 
-}
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void getNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public void setNombre (String nombre){
+    this.nombre = nombre;
+  }
+
+
+  public abstract String hacersonido();
+  public abstract String comer();
+
+
+};
+
